d2367cf4e34501413387a385619115f263c99aa0
tarcsi <mateplayshun@gmail.com>
2022-01-28 09:26:28 +0100

    Második feladat.

---
+++ 2022-01-28_Opening-weekend/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_01_28_Opening_weekend
+{
+    class Film
+    {
+        public string eredetiCim { get; set; }
+        public string magyarCim { get; set; }
+        public string bemutato { get; set; }
+        public string forgalmazo { get; set; }
+        public int bevetel { get; set; }
+        public int latogatok { get; set; }
+
+        public Film(string s) {
+            string[] db = s.Split(';');
+            eredetiCim = db[0];
+            magyarCim = db[1];
+            bemutato = db[2];
+            forgalmazo = db[3];
+            bevetel = int.Parse(db[4]);
+            latogatok = int.Parse(db[5]);
+        }
+    }
+}
--- 2022-01-28_Opening-weekend/OpeningWeekend.cs
+++ 2022-01-28_Opening-weekend/OpeningWeekend.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2022_01_28_Opening_weekend
 {
     class OpeningWeekend
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+            List<Film> filmek = new List<Film>();
+            File.ReadAllLines("nyitohetvege.txt").Skip(1).ToList().ForEach((s) => {
+                filmek.Add(new Film(s));
+            });
+            Console.ReadKey();
         }
     }
 }
