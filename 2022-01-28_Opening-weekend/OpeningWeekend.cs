047645a4f594e649739b014f1a86ecb99844a006
tarcsi <mateplayshun@gmail.com>
2022-01-31 13:32:17 +0100

    Ötös feladat.

--- 2022-01-28_Opening-weekend/OpeningWeekend.cs
+++ 2022-01-28_Opening-weekend/OpeningWeekend.cs
@@ -25,6 +25,15 @@ namespace _2022_01_28_Opening_weekend
             });
 
             Console.WriteLine($"4. feladat: UIP Duna Film forgalmazó 1. hetes bevételeinek összege: {uipBevetel.ToString("C2")}");
+
+            Film legtobbLatogato = filmek.OrderBy(f => f.latogatok).Last();
+
+            Console.WriteLine("5. feladat: Legtöbb látogató az első héten:");
+            Console.WriteLine($"\tEredeti cím: {legtobbLatogato.eredetiCim}");
+            Console.WriteLine($"\tMagyar cím: {legtobbLatogato.magyarCim}");
+            Console.WriteLine($"\tForgalmazó: {legtobbLatogato.forgalmazo}");
+            Console.WriteLine($"\tBevétel az első héten: {legtobbLatogato.bevetel.ToString("C2")}");
+            Console.WriteLine($"\tLátogatók száma: {legtobbLatogato.latogatok} fő");
             Console.ReadKey();
         }
     }
